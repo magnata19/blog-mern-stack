d08de70740f0b690ae918f6bb9e97a6ab0bac846
Davidson <dehpacifico@gmail.com>
2024-02-24 23:38:46 -0300

    configurar rotas

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-3xl text-blue-500'>app</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-out' element={<SignUp />}/>
+        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path='/projects' element={<Projects />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
