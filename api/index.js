77eadfe3430257e2debfed17c5c48fa7d36bfda2
Davidson <dehpacifico@gmail.com>
2024-02-25 23:25:34 -0300

    criar conexao com mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(
+  process.env.MONGO
+).then(() => {
+  console.log('Conectou ao banco com sucesso')
+}).catch((err) => {
+  console.log(err,"Deu ruim na conexão do banco.");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
