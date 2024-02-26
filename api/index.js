1c981095a4d90f1c72a459490c809e160287dab1
Davidson <dehpacifico@gmail.com>
2024-02-26 19:13:37 -0300

    criar cadastro de usuario no banco de dados

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 mongoose.connect(
   process.env.MONGO
@@ -20,3 +22,4 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes)
