d6b4c8440d66e681e1f9f2dc33b652fd8819dcf0
Davidson <dehpacifico@gmail.com>
2024-02-28 22:50:36 -0300

    criar estados de erros e loading e navigate

--- cliente/src/pages/SignUp.jsx
+++ cliente/src/pages/SignUp.jsx
@@ -1,27 +1,44 @@
-import { Button, Label, TextInput } from "flowbite-react";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
 
   const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  const navigate = useNavigate();
+  
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.id]: e.target.value})
+    setFormData({...formData, [e.target.id]: e.target.value.trim()})
   }
   
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if(!formData.username || !formData.email || !formData.password) {
+      setErrorMessage("Please fill out all fields.")
+    }
     try {
+      setLoading(true)
+      setErrorMessage(null)
       const res = await fetch('/api/auth/signup', {
         method:'POST',
         headers: {
           'Content-Type': 'application/json'},
           body: JSON.stringify(formData)
       });
-      const data = res.json();
+      const data = await res.json();
+      if(data.success === false ){
+        return setErrorMessage(data.message)
+      }
+      setLoading(false)
+      if(res.ok) {
+        navigate('/sign-in')
+      }
     } catch (error) {
-      
+      setErrorMessage(error.message)
+      setLoading(false)
     }
 
   }
@@ -65,8 +82,13 @@ const SignUp = () => {
               <Label value="Your pasword" />
               <TextInput type="password" placeholder="Password" id="password" onChange={handleChange}/>
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign Up
+            <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+              {loading ? (
+                <>
+                  <Spinner size='sm'/>
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : 'Sign Up'}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5 justify-center">
@@ -75,6 +97,11 @@ const SignUp = () => {
               Sign In
             </Link>
           </div>
+        {errorMessage && (
+          <Alert className="mt-5" color="failure">
+              {errorMessage}
+          </Alert>
+        )}
         </div>
       </div>
     </div>
